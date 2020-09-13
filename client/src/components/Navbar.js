fde6525c420f34edb140e4c79a4e20f169ac74ec
unknown <ahmedoprusevic@gmail.com>
2020-09-14 01:21:08 +0200

    fixing styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,8 +13,7 @@ import AddNewProject from './AddNewProject';
 import AddNewSkill from './AddNewSkill';
 import ContactMe from './ContactMe';
 import Alert from './Alert';
-import { useTheme } from '@material-ui/core/styles';
-import useStyles from '../styles/NavbarStyles';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -41,6 +40,104 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const drawerWidth = 240;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex'
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      backgroundColor: "#317BBE",
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+      [theme.breakpoints.down('sm')]: {
+        display: "none"
+      }
+    },
+    hide: {
+      display: 'none',
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      display: 'flex',
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: 'hidden',
+      width: theme.spacing(7) + 1,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing(9) + 1,
+      },
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1
+    },
+    list: {
+        height:"100%",
+        alignItems: "center",
+        justifyContent:"center",
+        display: "flex",
+        flexDirection: "column",
+        color: "#317BBE"
+    },
+    icons: {
+      width: "100%",
+      display:"flex",
+      justifyContent: "flex-end"
+    },
+    icon: {
+        color:"white",
+        margin: "0 0.5rem",
+        display: "flex",
+        textDecoration: "none",
+        alignItems: "center",
+        "&:hover": {
+          color: "#cddefa"
+        }
+    },
+    navIcon: {
+        color: "#317BBE"
+    },
+    link: {
+      width: "100%",
+      textDecoration: "none",
+      color: "#317BBE"
+    }
+  }));
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
