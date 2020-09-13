7ee32d3ac9ae444c43e66308012ac9e04653b433
unknown <ahmedoprusevic@gmail.com>
2020-09-13 21:29:00 +0200

    Prepare for deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 //Connect Database
@@ -9,7 +10,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API is Live'));
 
 // Define routes
 
@@ -17,6 +17,17 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/projects', require('./routes/api/projects'));
 app.use('/api/skills', require('./routes/api/skills'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+//Serve statuc assets in production
+
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
